edacd25fb8bff7f342e9e5f493573837b03be12b
EugeniaFranchi <mfranchi@fi.uba.ar>
2022-07-21 23:43:21 -0300

    cambio a hora de llegada tarde

--- set-triggers.py
+++ set-triggers.py
@@ -2,6 +2,7 @@ from pymongo import MongoClient
 import requests
 import json
 from dotenv import load_dotenv
+from datetime import timedelta
 import os
 
 load_dotenv()
@@ -20,9 +21,10 @@ def main():
 
   exams = col_exams.find()
   for exam in exams:
+    trigger_date = exam['start'] + timedelta(minutes=exam['startMinutesMargin'])
     data = json.dumps({
       "dag_run_id": str(exam.get('_id')),
-      "logical_date": str((exam.get('start')).isoformat())+'Z',
+      "logical_date": str(trigger_date.isoformat())+'Z',
     })
     
     response = requests.request("POST", url, headers=headers, data=data)
